1da31c2f0b0a1d335dd2700894d1f70918da95d8
alarmkim00 <alarmkim00@hotmai.com>
2016-07-19 10:18:18 +0900

    정적파일 생성

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello World!");
-})
+//app.get("/", function(req, res){
+//  res.send("Hello World!");
+//});
 
-app.listen(3000, function(){
+//app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+console.log(__dirname);
+app.listen(3001, function(){
   console.log("Server On!");
 });
